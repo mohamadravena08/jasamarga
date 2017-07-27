3685966daacca7f86d39b954de059cd97b4ff568
iqbalabiyoga <abiyoga.iqbal@gmail.com>
2017-07-27 10:40:03 +0700

    merge payroll.php

--- admin/production/payroll.php
+++ admin/production/payroll.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
     <!DOCTYPE html>
     <?php
       error_reporting(0);
@@ -16,17 +15,6 @@
       $updater=$updateBy['updater'];
       $waktu=$updateBy['timestamp'];
     ?>
-=======
-<?php
-error_reporting(0);
-session_start();
-include("../../library/koneksi.php");
-if(!isset($_SESSION["admin"])){
-  echo "<script language='javascript'>alert('Maaf Anda Belum Login!')</script>";
-  header("Location:../index.php");
-}
-?>
->>>>>>> d4cb30140a47951f50d8ea37bbf80565b1c9462f
 
     <html lang="en">
       <head>
